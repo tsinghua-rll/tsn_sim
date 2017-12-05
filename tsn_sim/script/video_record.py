cfade9d7e52cfc71acaeec020e902be0ce886386
Jeasine Ma <jeasinema@gmail.com>
2017-12-05 12:09:58 +0800

    video done

---
+++ tsn_sim/script/video_record.py
@@ -0,0 +1,43 @@
+
+from __future__ import print_function
+
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_converter:
+
+  def __init__(self):
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/camera/depth/image_raw", Image, self.callback)
+    self.cv_image = None 
+
+  def callback(self,data):
+    try:
+      self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    (rows,cols,channels) = self.cv_image.shape
+    if cols > 60 and rows > 60 :
+      cv2.circle(self.cv_image, (50,50), 10, 255)
+
+    cv2.imshow("Image window", self.cv_image)
+    cv2.waitKey(3)
+
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
